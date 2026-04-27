6923e18f00ea99bba9b34f1f3e228dcde7ad80ab
4budab1 <4budab1@gmail.com>
2026-04-27 15:54:42 +0300

    Revert "v4(stats hotfix)"
    This reverts commit 0b57a06c47203a2a83c42143ffdbba6b1dc0051e.

--- scripts/Game/MissionDataManager/ABU_FixMissionDataManager.c
+++ scripts/Game/MissionDataManager/ABU_FixMissionDataManager.c
@@ -19,11 +19,7 @@ modded class PS_MissionDataManager
 			if (!instigatorEntity)
 				return;
 
-			vector instigatorOrigin = instigatorEntity.GetOrigin();
-			vector targetOrigin = target.GetOrigin();
-			float distance = vector.Distance(instigatorOrigin, targetOrigin);
-
-			GetGame().GetCallqueue().Call(SaveDamageEventWithDistance, playerId, rplId, damageContext.damageValue, distance);
+			GetGame().GetCallqueue().Call(SaveDamageEvent, playerId, rplId, damageContext.damageValue);
 		}
 	}
 }
